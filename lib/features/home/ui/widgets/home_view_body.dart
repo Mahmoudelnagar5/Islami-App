55a29f5e3c11bbf52aaafa5eed44a186700d2bc6
mahmoudelnagar5 <ma982@gmail.com>
2024-11-08 01:07:13 +0200

    create prayer container and intl package

--- lib/features/home/ui/widgets/home_view_body.dart
+++ lib/features/home/ui/widgets/home_view_body.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
 
+import 'prayer_container.dart';
+
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
