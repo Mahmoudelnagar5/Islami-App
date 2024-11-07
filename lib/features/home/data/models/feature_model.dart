5f858017bc73378ab75946cb7f021594cad27414
mahmoudelnagar5 <ma982@gmail.com>
2024-11-08 01:37:19 +0200

    create feature item and feature model

---
+++ lib/features/home/data/models/feature_model.dart
@@ -0,0 +1,9 @@
+class FeatureModel {
+  final String title;
+  final String image;
+
+  const FeatureModel({
+    required this.title,
+    required this.image,
+  });
+}
---
+++ lib/features/home/ui/widgets/feature_item.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/theming/colors.dart';
+
+class FeatureItem extends StatelessWidget {
+  const FeatureItem({super.key, required this.image, required this.title});
+  final String image;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(horizontal: 5.w),
+            decoration: BoxDecoration(
+              color: AppColors.lightPurple,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: CircleAvatar(
+              radius: 30.r,
+              backgroundColor: AppColors.lightPurple,
+              // child: AssetImage('assets/images/prayer_timing.png'),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 16.sp,
+              fontWeight: FontWeight.bold,
+              color: AppColors.textColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
