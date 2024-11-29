f8d5ff3825f73f2ab8c8ad7c0107ec82baf01781
mahmoudelnagar5 <elnagarm852@gmail.com>
2024-11-29 15:49:34 +0200

    finish repo and repo impl of recitations

--- lib/core/utils/api_services.dart
+++ lib/core/utils/api_services.dart
@@ -3,9 +3,22 @@ import 'package:dio/dio.dart';
 class ApiServices {
   static const String baseUrl = 'https://api.alquran.cloud/v1/';
   Future<Map<String, dynamic>> getSurahDetails(int id) async {
-    var response = Dio().get('${baseUrl}surah/$id/ar.alafasy');
-    return response.then((value) {
-      return value.data;
-    });
+    var response = await Dio().get('${baseUrl}surah/$id/ar.alafasy');
+    return response.data;
+  }
+
+  Future<Map<String, dynamic>> getAllRecitations() async {
+    var respone = await Dio()
+        .get('https://api.quran.com/api/v4/resources/recitations?language=ar');
+    // print(respone.data);
+    return respone.data;
+  }
+
+  Future<Map<String, dynamic>> getSurahAudio(
+      String reciterId, String chapterId) async {
+    var respone = await Dio().get(
+        'https://api.quran.com/api/v4/chapter_recitations/$reciterId/$chapterId');
+    // print(respone.data);
+    return respone.data;
   }
 }
